575ed16d385844cd24cc82910885a5ab07709f71
Devat Karetha <devat.karetha@viitor.cloud>
2019-11-11 15:16:43 +0530

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# Linkedin-Laravel-6.0
---
+++ config/services.php
@@ -0,0 +1,39 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Third Party Services
+    |--------------------------------------------------------------------------
+    |
+    | This file is for storing the credentials for third party services such
+    | as Mailgun, Postmark, AWS and more. This file provides the de facto
+    | location for this type of information, allowing packages to have
+    | a conventional file to locate the various service credentials.
+    |
+    */
+
+    'mailgun' => [
+        'domain' => env('MAILGUN_DOMAIN'),
+        'secret' => env('MAILGUN_SECRET'),
+        'endpoint' => env('MAILGUN_ENDPOINT', 'api.mailgun.net'),
+    ],
+
+    'postmark' => [
+        'token' => env('POSTMARK_TOKEN'),
+    ],
+
+    'ses' => [
+        'key' => env('AWS_ACCESS_KEY_ID'),
+        'secret' => env('AWS_SECRET_ACCESS_KEY'),
+        'region' => env('AWS_DEFAULT_REGION', 'us-east-1'),
+    ],
+
+    'linkedin' => [
+        'client_id' => '8120utjthv77el',
+        'client_secret' => 'KwFy85U1BfX5iVcM',
+        'redirect' => 'http://127.0.0.1:8000/callback/linkedin',
+    ], 
+
+];
